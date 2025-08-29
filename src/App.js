13bb861b909330fbc0c317188efbb1c57d2bb465
Suban2108 <Suban14925@gmail.com>
2025-08-29 11:25:17 +0530

    assign-5

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ProductList from "./components/ProductList";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+    <div className="container">
+      <h1>🧺 LaundryPro | React Custom Hook Demo</h1>
+      </div>
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
